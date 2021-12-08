f8a9467eaa73fc6b30aabd240ae975a3887315e9
Batuhan Dursun <94258091+BatuDursun@users.noreply.github.com>
2021-12-09 01:12:12 +0300

    Add files via upload

---
+++ Zar Oyunu/Zar Oyunu/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace Zar_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(22, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Oyun kaç puanda bitsin?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(308, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(76, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(322, 100);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(59, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Oyuncu 1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(310, 292);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 44);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Oyuncu 2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Oyuncu 1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(310, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Oyuncu 2:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(419, 260);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "0";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(161, 399);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(189, 44);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Oyuna tekrar başla";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(134, 362);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(264, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Oyuncu 1 kazandı.Tebrikler :)";
+            this.label6.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(500, 461);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Zar Oyunu/Zar Oyunu/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zar_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //GLOBAL VARİABLES
+        Random rnd = new Random();
+        int oyuncu1Puan;
+        int oyuncu2Puan;
+        int a, b;
+
+        private void zarAt()
+        {
+            a = rnd.Next(1, 7);
+            b = rnd.Next(1, 7);
+            //oyuncu 1 için zar görselleri
+            if (a==1)
+            {
+                pictureBox1.Image = Image.FromFile(@"C:\Users\monster\source\repos\Zar Oyunu\Zar Oyunu\bin\Debug\image\1.png");
+            }
+            if (a == 2)
+            {
+                pictureBox1.Image = Image.FromFile(@"C:\Users\monster\source\repos\Zar Oyunu\Zar Oyunu\bin\Debug\image\2.png");
+            }
+            if (a == 3)
+            {
+                pictureBox1.Image = Image.FromFile(@"C:\Users\monster\source\repos\Zar Oyunu\Zar Oyunu\bin\Debug\image\3.png");
+            }
+            if (a == 4)
+            {
+                pictureBox1.Image = Image.FromFile(@"C:\Users\monster\source\repos\Zar Oyunu\Zar Oyunu\bin\Debug\image\4.png");
+            }
+            if (a == 5)
+            {
+                pictureBox1.Image = Image.FromFile(@"C:\Users\monster\source\repos\Zar Oyunu\Zar Oyunu\bin\Debug\image\5.png");
+            }
+            if (a == 6)
+            {
+                pictureBox1.Image = Image.FromFile(@"C:\Users\monster\source\repos\Zar Oyunu\Zar Oyunu\bin\Debug\image\6.png");
+            }
+            //oyuncu 2 için zar görselleri
+            if (b == 1)
+            {
+                pictureBox2.Image = Image.FromFile(@"C:\Users\monster\source\repos\Zar Oyunu\Zar Oyunu\bin\Debug\image\1.png");
+            }
+            if (b == 2)
+            {
+                pictureBox2.Image = Image.FromFile(@"C:\Users\monster\source\repos\Zar Oyunu\Zar Oyunu\bin\Debug\image\2.png");
+            }
+            if (b == 3)
+            {
+                pictureBox2.Image = Image.FromFile(@"C:\Users\monster\source\repos\Zar Oyunu\Zar Oyunu\bin\Debug\image\3.png");
+            }
+            if (b == 4)
+            {
+                pictureBox2.Image = Image.FromFile(@"C:\Users\monster\source\repos\Zar Oyunu\Zar Oyunu\bin\Debug\image\4.png");
+            }
+            if (b == 5)
+            {
+                pictureBox2.Image = Image.FromFile(@"C:\Users\monster\source\repos\Zar Oyunu\Zar Oyunu\bin\Debug\image\5.png");
+            }
+            if (b == 6)
+            {
+                pictureBox2.Image = Image.FromFile(@"C:\Users\monster\source\repos\Zar Oyunu\Zar Oyunu\bin\Debug\image\6.png");
+            }
+            
+        }
+        private void oyuncuSkor()
+        {
+            if (oyuncu1Puan >= Convert.ToInt32(textBox1.Text))
+            {
+
+                label6.Visible = true;
+                button4.Visible = true;
+                button2.Visible = false;
+            }
+            if (oyuncu2Puan >= Convert.ToInt32(textBox1.Text))
+            {
+
+                label6.Visible = true;
+                label6.Text = "Oyuncu 2 kazandı.Tebrikler :)";
+                button4.Visible = true;
+                button1.Visible = false;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Enabled = false;
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            button1.Enabled = false;
+            button2.Enabled = true;
+            zarAt();
+            oyuncu1Puan = oyuncu1Puan + a+b;
+            label3.Text = oyuncu1Puan.ToString();
+            oyuncuSkor();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            zarAt();
+            oyuncu2Puan = oyuncu2Puan + b+a;
+            label5.Text = oyuncu2Puan.ToString();
+            oyuncuSkor();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "100";
+            textBox1.Enabled = true;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+
+            button1.Visible = true;
+            button2.Visible = true;
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+            label3.Text = "0";
+            label5.Text = "0";
+            oyuncu1Puan = 0;
+            oyuncu2Puan = 0;
+            button4.Visible = false;
+            label6.Visible = false;
+        }
+    }
+}
